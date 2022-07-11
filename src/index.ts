adfcd80e4c2b500a0ae508e4948d09d5f8c7274e
renato3x <pereirarenato21@gmail.com>
2022-07-11 00:40:33 -0300

    base concluida

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-import fs from 'fs'
-import path from 'path';
-import {  } from 'rxjs';
-import chalk from 'chalk'
+import fs from 'fs' // manipula os arquivos do sistema
+import path from 'path' // para gerar o caminho para os arquivos
+import chalk from 'chalk' // para colorir o console :)
+import { Observable } from 'rxjs'
 
-const isCSS = /.+\s*{\s*(.+:\s*.+;\s*)*}\s*/gmi
-const isHTML = /^<!DOCTYPE html>/ig
+const isCSS = /.+\s*{\s*(.+:\s*.+;\s*)*}\s*/gmi // expressão regular para pegar arquivos CSS
+const isHTML = /^<!DOCTYPE html>/ig // expressão regular para pegar arquivos HTML
 
-const filePaths: string[] = [
-  path.join(__dirname, 'arquivo1.txt'),
-  path.join(__dirname, 'arquivo2.html'),
-  path.join(__dirname, 'arquivo3.css'),
-  path.join(__dirname, 'arquivo4.txt'),
+const files: string[] = [
+  path.join(__dirname, 'texto1.txt'),
+  path.join(__dirname, 'html1.html'),
+  path.join(__dirname, 'css1.css'),
+  path.join(__dirname, 'texto2.txt'),
+  path.join(__dirname, 'html2.html'),
+  path.join(__dirname, 'css2.css'),
+  path.join(__dirname, 'texto3.txt'),
+  path.join(__dirname, 'html3.html'),
+  path.join(__dirname, 'css3.css'),
+  path.join(__dirname, 'texto4.txt'),
+  path.join(__dirname, 'html4.html'),
+  path.join(__dirname, 'css4.css'),
 ]
 
 function readFiles() {
-
+  
 }
