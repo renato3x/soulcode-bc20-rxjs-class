3a578428cbaff3f6c90b83507e51f473cd852b58
renato3x <pereirarenato21@gmail.com>
2022-07-12 14:04:07 -0300

    operadores

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import fs, { read } from 'fs' // manipula os arquivos do sistema
 import path from 'path' // para gerar o caminho para os arquivos
 import chalk from 'chalk' // para colorir o console :)
-import { Observable, Subject, Subscriber } from 'rxjs'
+import { catchError, filter, first, last, map, Observable, of, retry, Subject, Subscriber, take, throwError, timeout } from 'rxjs'
 
 const isCSS = /.+\s*{\s*(.+:\s*.+;\s*)*}\s*/gmi // expressão regular para pegar arquivos CSS
 const isHTML = /^<!DOCTYPE html>/ig // expressão regular para pegar arquivos HTML
@@ -9,7 +9,7 @@ const isHTML = /^<!DOCTYPE html>/ig // expressão regular para pegar arquivos HT
 const files: string[] = [
   path.join(__dirname, 'texto1.txt'),
   path.join(__dirname, 'html1.html'),
-  path.join(__dirname, 'css1.css'),
+  path.join(__dirname, 'css1.scss'),
   path.join(__dirname, 'texto2.txt'),
   path.join(__dirname, 'html2.html'),
   path.join(__dirname, 'css2.css'),
@@ -66,7 +66,7 @@ function readFiles(arquivos: string[], hot: boolean) {
       }
 
       i++
-    }, 2500)
+    }, 3000)
   })
 
   if (hot == true) {
@@ -93,7 +93,42 @@ const leitor$ = readFiles(files, true)
  * 2° -> Erro (error)
  * 3° -> Concluído (complete)
  */
-leitor$.subscribe(
+leitor$
+.pipe(
+/*   map((conteudo) => {
+    return conteudo.length
+  }),
+  map((qtdCaracteres) => {
+    return qtdCaracteres * 3
+  }) */
+/*   filter((conteudo) => {
+    return isCSS.test(conteudo)
+  }) */
+  /* filter((conteudo) => {
+    return isHTML.test(conteudo)
+  }),
+  map((html) => {
+    return html.length
+  }) */
+  // take(1)
+  // first()
+  /* first((conteudo) => {
+    return !isCSS.test(conteudo) && !isHTML.test(conteudo)
+  }) */
+  // last()
+  /* last((conteudo) => {
+    return isHTML.test(conteudo)
+  }) */
+  /* catchError((erro) => {
+    console.log(chalk.italic(chalk.red('ERRO OCORREU NO OBSERVABLE')))
+
+    // return of(erro)
+    return throwError(erro)
+  }) */
+  // retry(5)
+  timeout(5000)
+)
+.subscribe(
   // next
   (conteudoArquivo) => {
     console.log(chalk.green('SUB 1 - Texto lido com sucesso!\n'))
@@ -109,22 +144,3 @@ leitor$.subscribe(
     console.log(chalk.blue('Todos os arquivos foram lidos :)'))
   }
 )
-
-setTimeout(() => {
-  leitor$.subscribe(
-    // next
-    (conteudoArquivo) => {
-      console.log(chalk.green('SUB 2 - Texto lido com sucesso!\n'))
-      console.log(conteudoArquivo + '\n')
-    },
-    // error
-    (erro) => {
-      console.log(chalk.red('Ocorreu um erro na execução\n'))
-      console.log(erro)
-    },
-    //complete
-    () => {
-      console.log(chalk.blue('Todos os arquivos foram lidos :)'))
-    }
-  )
-}, 10000)
